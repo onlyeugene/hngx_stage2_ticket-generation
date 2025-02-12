0c0af295daa16bf9c2a18b0606437b82b2b6801a
onlyeugene <eugeneokogun@gmail.com>
2025-02-12 10:24:52 +0100

    fix:fixed a change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ import Tickets from './tickets'
 
 const Navbar = () => {
   return (
-    <div className='py-5 fixed w-full '>
+    <div className='py-5 fixed w-full z-20'>
       <Container>
         <div className='flex justify-between items-center border px-5 py-3 rounded-2xl border-primary bg-[#05252c82] z-10'>
         <Logo />
